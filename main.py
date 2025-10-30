0faffdb24dbdd373a9d38fe77fb72d20ec3410df
SAKSHAM AGARWAL <agarwalsaksham11@gmail.com>
2025-10-30 22:10:03 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
-# from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 # from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 # from src.textSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
@@ -15,3 +15,13 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME="Data Transformation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
